e5c44a3e93bfa907a979d49e99b110e3dc001c92
vaibhavshukla <shukla.v@husky.neu.edu>
2018-07-23 11:35:38 -0400

    Added ModuleListItem

---
+++ src/components/ModuleListItem.js
@@ -0,0 +1,33 @@
+import React,{ Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ModuleListItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.delModule = this.delModule.bind(this);
+    }
+
+
+    delModule(event){
+        console.log(event.target.value);
+        console.log(this.props.moduleId);
+        this.props.delete(this.props.moduleId);
+    }
+
+    render(){
+        return  (<li className="list-group-item">
+                <Link to= {`/course/${this.props.courseId}/module/${this.props.moduleId}`}>
+                    {this.props.title}
+                </Link>
+                <span className="float-right">
+                <button id={this.props.courseId} onClick={this.delModule} type="button" className="btn btn-danger">
+                    <i className="fa fa-trash"></i>
+                </button>
+              </span>
+            </li>
+        );
+    }
+}
+
+export default ModuleListItem;
